f325d1b4f44264422b5d5568a9f1d3440d78a037
kakaumesh <140509384+kakaumesh@users.noreply.github.com>
2024-12-02 13:33:05 +0530

    Update githubdemo3.java

--- githubdemo3/src/program/githubdemo3.java
+++ githubdemo3/src/program/githubdemo3.java
@@ -5,6 +5,7 @@ public class githubdemo3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Niranjan");
+		System.out.println("Hiremath");
 
 	}
 
